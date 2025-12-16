46bb27a09a3b1af36dc68c33b4ae6664588899e4
Gabriel Anderson <145013468+gamakagami@users.noreply.github.com>
2025-12-17 06:21:17 +0700

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,4 +5,12 @@ const router = express.Router();
 
 router.get('/:id', getUser);
 
+router.post('/:id/favorite', auth, async (req, res) => {
+  const post = await Post.findById(req.params.id);
+   post.favorites.push(req.user.id);
+   await post.save();
+   res.json({ success: true });
+ });
+
+
 export default router;
